0e5b1bec55936bea059784c6954863e5e376ab1b
Yaman Katby <m.yaman.katby@gmail.com>
2023-03-24 19:10:52 +0300

    Initial Commit

---
+++ PrayerTimes/PrayerTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimesApp.swift
+//  PrayerTimes
+//
+//  Created by Yaman Katby on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
